d16f09bc88b32d18d068a4a21b8a109da9dfb32d
Capet-Zeyu-Lin <zlin0089@student.monash.edu>
2025-08-18 19:25:50 +1000

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# week4efolio
+# week3
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/components/Form.vue
@@ -0,0 +1,191 @@
+
+
+<script setup>
+import { ref } from 'vue';
+
+const formData = ref({
+  username: '',
+  password: '',
+  isAustralian: false,
+  reason: '',
+  gender: ''
+});
+
+const submittedCards = ref([]);
+
+const submitForm = () => {
+  validateName(true)
+  validatePassword(true)
+  validateGender(true)
+  if(!errors.value.username && !errors.value.password && !errors.value.gender) {
+    submittedCards.value.push({
+      ...formData.value
+    });
+    clearForm()
+  }
+
+};
+
+const clearForm = () => {
+  formData.value = {
+    username: '',
+    password: '',
+    isAustralian: false,
+    reason: '',
+    gender: ''
+  }
+}
+
+const errors = ref({
+  username: null,
+  password: null,
+  resident:null,
+  gender: null,
+  reason: null
+})
+
+const validateName = (blur) => {
+  if(formData.value.username.length<3){
+    if(blur) errors.value.username = "Name must be at least 3 characters";
+  }else {
+    errors.value.username = null;
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password;
+  const minLength = 8;
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  if(password.length < minLength){
+    if(blur) errors.value.password = `Password must be at least ${minLength} characters`;
+  } else if(!hasUppercase){
+    if(blur) errors.value.password = `Password must contain at least one uppercase letter`;
+  } else if(!hasLowercase){
+    if(blur) errors.value.password = `Password must contain lowercase letter`;
+  } else if(!hasSpecialChar){
+    if(blur) errors.value.password = `Password must contain special char`;
+  } else {
+    errors.value.password = null;
+  }
+
+}
+
+const validateGender = (blur) => {
+  const gender = formData.value.gender;
+  if(!gender){
+    if(blur) errors.value.gender = "Please select a  gender at least";
+  } else {
+    errors.value.gender = null;
+  }
+}
+
+
+</script>
+
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="text-center">
+          User Information Form
+        </h1>
+        <form @submit.prevent="submitForm">
+          <div class="row mb-3">
+            <div class="col-md-6">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username"
+                     @blur= "() => validateName(true)"
+                     @input= "() => validateName(false)"
+                     v-model="formData.username"/>
+              <div v-if="errors.username" class="text-danger">{{errors.username}}</div>
+            </div>
+            <div class="col-md-6">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password"
+                     @blur= "() => validatePassword(true)"
+                     @input= "() => validatePassword(false)"
+                     v-model="formData.password"/>
+              <div v-if="errors.password" class="text-danger">{{errors.password}}</div>
+            </div>
+          </div>
+
+      <div class="row mb-3">
+        <div class="col-md-6">
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian">
+            <label class="form-check-label" for="isAustralian">Australia Resident?</label>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <label for="gender" class="form-label">Gender</label>
+          <select class="form-select" id="gender"
+                  @blur = "() => validateGender(true)"
+                  @change = "() => validateGender(false)"
+                  v-model="formData.gender">
+            <option disabled value="">-- Please select gender --</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="reason" class="form-label">Reason for joining</label>
+        <textarea class="form-control" id="reason"  rows="3" v-model="formData.reason"></textarea>
+      </div>
+      <div class="text-center">
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
+      </div>
+      </form>
+
+
+    </div>
+  </div>
+    <div class="row mt-5" v-if="submittedCards.length">
+      <div class="d-flex flex-row justify-content-start">
+        <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
+          <div class="card-header">
+            User Information
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275FDA;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+.list-group-item {
+  padding: 10px;
+}
+</style>
